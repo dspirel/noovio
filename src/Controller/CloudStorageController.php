f6a0b7cb871f4ffe0210373602c55a544910cea4
Davor Spirelja <davor.spirelja@gmail.com>
2025-07-22 15:18:06 +0200

    add homepage

--- src/Controller/CloudStorageController.php
+++ src/Controller/CloudStorageController.php
@@ -6,6 +6,7 @@ use App\Service\GoogleCloudStorageService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 
@@ -13,6 +14,12 @@ use function Symfony\Component\Clock\now;
 
 final class CloudStorageController extends AbstractController
 {
+    #[Route('/', name: 'app_cloud_storage')]
+    public function home(): Response
+    {
+        return $this->render('index');
+    }
+
     #[Route('/storage/getSignedUrls', name: 'app_cloud_storage')]
     public function index(
         GoogleCloudStorageService $googleCloudStorageService,
